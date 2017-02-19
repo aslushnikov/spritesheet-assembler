cf205de7205d060349f7070c8331cec8197451ec
Andrey Lushnikov <lushnikov@chromium.org>
2017-02-18 20:22:04 -0800

    simple svg test

--- tests/01-svg-simple/arguments.js
+++ tests/01-svg-simple/arguments.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = function(outputFolder) {
     return [
-        '-i', path.join(__dirname, 'input'),
+        '-i', __dirname,
         '-o', path.join(outputFolder, 'spritesheet.svg'),
         '-d', path.join(outputFolder, 'descriptors.js'),
         '-t', 'devtools'
